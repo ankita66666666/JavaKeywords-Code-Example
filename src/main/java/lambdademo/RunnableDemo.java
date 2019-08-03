24d68d53771308c10a3150b0c07707ff7727a868
Ankita <kumariankita.gaurav@cgi.com>
2019-08-03 17:10:58 +0530

    completed Runtime polymorphism

---
+++ src/main/java/lambdademo/RunnableDemo.java
@@ -0,0 +1,9 @@
+package lambdademo;
+
+    public class RunnableDemo implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("New Thread running");
+        }
+    }
+
