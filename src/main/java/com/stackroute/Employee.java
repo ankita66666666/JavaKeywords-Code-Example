4624b36da64eb98ba3832f75135d935b12bd2604
Ankita <kumariankita.gaurav@cgi.com>
2019-08-03 11:45:21 +0530

    Done ObjectAndClass

---
+++ src/main/java/com/stackroute/Employee.java
@@ -0,0 +1,9 @@
+package com.stackroute;
+
+//Defining a Employee class.
+
+public class Employee {
+    //field or data member or instance variable
+    int id=23456;
+    String name="Ankita";
+}
---
+++ src/main/java/com/stackroute/ObjectAndClass.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+public class ObjectAndClass {
+    //Java Program to illustrate how to define a class and fields
+        //creating main method inside the Employee class
+        public static void main(String args[]){
+            //Creating an object or instance
+            Employee e=new Employee();//creating an object of Employee
+            //Printing values of the object
+            System.out.println(e.id);//accessing member through reference variable
+            System.out.println(e.name);
+        }
+    }
+
